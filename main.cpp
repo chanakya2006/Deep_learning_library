1389875a354764eed1abdf8fe96198a04abd2abc
chanakya2006 <chanakya.harit@gmail.com>
2026-02-28 01:09:33 +0530

    Benchmarked the already implemented matmul_2D method

--- main.cpp
+++ main.cpp
@@ -1,149 +1,57 @@
-#include "loss/bce.hpp"
-#include "loss/mce.hpp"
-#include "loss/mse.hpp"
-#include "model/sequential.hpp"
-#include "nn/activations.hpp"
-#include "nn/dense.hpp"
-#include "nn/layer.hpp"
-#include "optim/Adam.hpp"
 #include "tensor/tensor.hpp"
-#include "utils/helpers.hpp"
-#include <climits>
-#include <cstddef>
-#include <fstream>
+#include <cstdlib>
 #include <iostream>
-#include <limits>
-#include <memory>
-#include <ratio>
+#include <string>
 #include <vector>
+
+// command :
+// perf stat -e cycles,instructions,cache-references,cache-misses ./train
 
 using namespace std;
 
-string path_to_model = "trained_models/MNIST_sequential.dat";
-string path_to_dataset = "datasets/mnist_train.csv";
-bool use_saved_model = false;
+#define DEFAULT_SIZE 2048
 
-vector<float> label_to_encoding(float label) {
-  vector<float> out(10, 0);
-  out[label] = 1;
-  return out;
+int size_of_mat;
+
+void randomize(Tensor &t) {
+  vector<float> &data = *t.get_data_pointer();
+  for (float &x : data) {
+    x = ((float)rand() / RAND_MAX) - 0.5f;
+  }
 }
 
-int encoding_to_label(vector<float> encoding) {
-  float max_value = 0;
-  int max_value_at = 0;
-  for (size_t i = 0; i < encoding.size(); i++) {
-    if (encoding[i] > max_value) {
-      max_value = encoding[i];
-      max_value_at = i;
-    }
-  }
-  return max_value_at;
-}
+int main(int argc, char *argv[]) {
+  if (argc == 2)
+    size_of_mat = stoi(argv[1]);
+  else
+    size_of_mat = DEFAULT_SIZE;
 
-void train() {
-  Sequential model;
+  Tensor t1{{size_of_mat, size_of_mat}};
+  Tensor t2{{size_of_mat, size_of_mat}};
+  randomize(t1);
+  randomize(t2);
 
-  if (use_saved_model) {
-    model = load_Sequential_model(path_to_model);
-  } else {
-    model.add(make_unique<Dense>(783, 256));
-    model.add(make_unique<Tanh>());
-    model.add(make_unique<Dense>(256, 128));
-    model.add(make_unique<Tanh>());
-    model.add(make_unique<Dense>(128, 10));
-    model.add(make_unique<Softmax>());
-  }
-
-  vector<Tensor *> parameters = model.get_parameters();
-
-  MCE mce(10);
-
-  Adam adam;
-
-  csv_loader loader(path_to_dataset, 2);
-
-  for (int epoch = 0; epoch < 2; epoch++) {
-    float epoch_loss = 0;
-
-    for (size_t i = 0; i < loader.num_of_rows; i++) {
-
-      ::data output_and_input = loader.load_next();
-      ::data input_and_output{output_and_input.output,
-                              label_to_encoding(output_and_input.input[0])};
-
-      Tensor x({1, int(input_and_output.input.size())}, input_and_output.input,
-               false);
-      Tensor y_true({1, int(input_and_output.output.size())},
-                    input_and_output.output, false);
-
-      Tensor y_pred = model.forward(x);
-
-      Tensor loss = mce.apply(y_pred, y_true);
-      epoch_loss += loss.get_data()[0];
-
-      cout << "Row : " << i << "\tThe loss was : " << loss.get_data()[0]
-           << endl;
-
-      loss.backward();
-      adam.step(parameters);
-      adam.zero_grad(parameters);
-    }
-
-    loader.reset();
-
-    cout << "Epoch " << epoch << " loss = " << epoch_loss / loader.num_of_rows
-         << endl;
-  }
-
-  // Saving model
-  model.save(path_to_model);
-}
-
-float get_accuracy() {
-  size_t sample_size = 6000;
-
-  Sequential model = load_Sequential_model(path_to_model);
-
-  csv_loader loader(path_to_dataset, 2);
-
-  float correct = 0;
-
-  for (size_t i = 0; i < sample_size; i++) {
-    ::data output_and_input = loader.load_next();
-    ::data input_and_output{output_and_input.output,
-                            label_to_encoding(output_and_input.input[0])};
-
-    Tensor x{
-        {1, int(input_and_output.input.size())}, input_and_output.input, false};
-    Tensor y_pred = model.forward(x);
-
-    int pred_num = encoding_to_label(y_pred.get_data());
-    int true_num = output_and_input.input[0];
-
-    if (pred_num == true_num) {
-      correct++;
-    }
-  }
-  loader.reset();
-
-  return float(correct / sample_size) * 100;
-}
-
-int main() {
-
-  // To download the mnist_train.csv dataset please visit
-  // https://www.kaggle.com/datasets/oddrationale/mnist-in-csv?select=mnist_train.csv
-
-  // NOTE : Please be sure to remove the first row in the datatset
-
-  train();
-
-  float accuracy = get_accuracy();
-  cout << "accuracy :  " << accuracy << endl;
+  cout << "Matmul begins" << endl;
+  Tensor out = t1.matmul_2D(t2);
+  cout << "Matmul ended" << endl;
 
   return 0;
 }
+
+/*
+ 1) buildin tensor matmul_2D
+
+    234,948,103,408      cycles
+    61,458,759,363      instructions  #    0.26  insn per cycle
+    8,695,158,605      cache-references
+    5,567,695,159      cache-misses  #   64.03% of all cache refs
+
+      56.351237922 seconds time elapsed
+
+      56.199030000 seconds user
+       0.092971000 seconds sys
+
+ */
 
 // Implement :
 // RSMprop
